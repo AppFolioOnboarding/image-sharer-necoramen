a0a2fc97017194da4e4eaad2d07eb6d02831e99b
Wenrui Wang <wenrui.wang@appfolio.com>
2021-03-25 15:05:43 -0400

    display error message for empty link submitted

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,18 @@
 class Image < ApplicationRecord
+  validate :url_link_to_valid_image
+
+  private
+
+  def url_link_to_valid_image
+    if !url.present?
+      errors.add(:url, 'cannot be empty')
+    elsif !obj_is_valid_image?(url)
+      errors.add(:url, 'is not valid')
+    end
+  end
+
+  def obj_is_valid_image?(url)
+    url.present? && url.length > 4
+    # TODO: check if url points to an image
+  end
 end
