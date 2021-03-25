aeadab032582e04b77b801cd28aa08c14b5e976f
Wenrui Wang <wenrui.wang@appfolio.com>
2021-03-25 11:42:43 -0400

    get link submitted through form and display the image if the link is saved

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,24 @@
 class ImagesController < ApplicationController
-  def new; end
+  def new
+    @image = Image.new
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to @image
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:url)
+  end
 end
