0fc619a17a44d1171f8d23a4ac7287476f0738a6
Wenrui Wang <wenrui.wang@appfolio.com>
2021-03-26 15:11:21 -0400

    use gem validate_url for url validation

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,18 +1,5 @@
 class Image < ApplicationRecord
-  validate :url_link_to_valid_image
 
-  private
+  validates :url, url: true
 
-  def url_link_to_valid_image
-    if !url.present?
-      errors.add(:url, 'cannot be empty')
-    elsif !obj_is_valid_image?(url)
-      errors.add(:url, 'is not valid')
-    end
-  end
-
-  def obj_is_valid_image?(url)
-    url.present? && url.length > 4
-    # TODO: check if url points to an image
-  end
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "successfully saves an image" do
+    image = Image.new
+    image.url = 'https://google.com'
+    assert_difference('Image.count', 1) do
+      image.save!
+    end
+  end
+
+  test 'tries to save an image with invalid url' do
+    image = Image.new
+    image.url = 'google.com'
+    refute_predicate(image, :valid?)
+  end
 end
