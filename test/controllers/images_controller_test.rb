e72a0e07be22309a198b4ef9fbf064badc679e78
Wenrui Wang <wenrui.wang@appfolio.com>
2021-03-26 20:02:09 -0400

    add controllers for Image allowing users to submit links

---
+++ test/controllers/images_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class ImagesControllerTest < ActionDispatch::IntegrationTest
+  valid_url = 'https://robbreport.com/wp-content/uploads/2019/05/sbsailingcenter_sbinnview_photobyblakebronstad.jpg.jpg?w=1000'
+  invalid_url = valid_url[6..valid_url.length]
+
+  test 'visit /image/new' do
+    get new_image_path
+    assert_response :ok
+    assert_select('h1', 'Submit a new link')
+  end
+
+  test 'display an image' do
+    image = Image.create!(url: valid_url)
+    get image_path(image)
+    assert_response :ok
+    assert_select('h1', "Showing image #{image.id}")
+    assert_select('img[src=?]', valid_url)
+  end
+
+  test 'create an image with valid url' do
+    post '/images', params: { image: { url: valid_url } }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :ok
+    assert_select('img[src=?]', valid_url)
+  end
+
+  test 'create an image with invalid url' do
+    post '/images', params: { image: { url: invalid_url } }
+    assert_response :ok
+    assert_select('div', 'Url is not a valid URL')
+  end
+end
