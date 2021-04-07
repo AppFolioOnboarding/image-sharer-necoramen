b7c56fe165024988bc43319d670be3f19421447a
Wenrui Wang <wenrui.wang@appfolio.com>
2021-04-07 18:03:17 -0400

    Add FeedbackForm component to each page

--- app/javascript/image_sharer/index.js
+++ app/javascript/image_sharer/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Footer from './Footer.jsx';
+import FeedbackForm from './FeedbackForm.js';
+import FeedbackStore from './stores/FeedbackStore';
+
+const store = new FeedbackStore();
+
+ReactDOM.render(
+  <FeedbackForm store={store} />,
+  document.getElementById('feedback-form')
+);
 
 ReactDOM.render(
   <Footer />,
