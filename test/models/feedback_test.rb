ea86b0158e3b5763e1765d496d06f8635bb1e626
Wenrui Wang <wenrui.wang@appfolio.com>
2021-04-08 18:57:57 -0400

    Add model Feedback

---
+++ test/models/feedback_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class FeedbackTest < ActiveSupport::TestCase
+  test 'successfully saves an image' do
+    assert_difference('Feedback.count', 1) do
+      Feedback.create!(username: 'user1', comment: 'comment1')
+    end
+  end
+
+  test 'does not save empty comments' do
+    assert_no_difference('Feedback.count') do
+      Feedback.create(username: 'user1', comment: '')
+    end
+  end
+end
